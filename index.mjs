c9087906f82e93634b22ae679d445d8d165e850d
GitHub30 <funaox@gmail.com>
2026-03-28 04:13:21 +0000

    Revert "refactor: streamline browser instance management in screenshot generation"
    This reverts commit 495ec36cee547a165ff79f18dcf4ccf3a7d19ac9.

--- index.mjs
+++ index.mjs
@@ -3,6 +3,17 @@ import puppeteer from 'puppeteer'
 
 const app = express()
 
+let browser = null
+
+async function getBrowser() {
+  if (!browser || !browser.connected) {
+    browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    })
+  }
+  return browser
+}
+
 app.use((req, res, next) => {
   res.set('Access-Control-Allow-Origin', '*')
   res.set('Access-Control-Allow-Methods', '*')
@@ -21,12 +32,8 @@ app.get('/', async (req, res) => {
     return res.sendStatus(204)
   }
 
-  let browser = null
+  const page = await getBrowser().then(b => b.newPage())
   try {
-    browser = await puppeteer.launch({
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
-    })
-    const page = await browser.newPage()
     page.setUserAgent({ userAgent: 'Mozilla/5.0 (Windows NT 10.0 Win64 x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36' })
     await page.goto('https://www.digicert.com/help/?' + new URLSearchParams({ host }), { waitUntil: 'networkidle2', timeout: 10000 })
     await page.addStyleTag({ content: 'header, #transcend-consent-manager { display: none !important }' })
@@ -52,9 +59,7 @@ app.get('/', async (req, res) => {
     console.error(error)
     res.status(500).send(error.message)
   } finally {
-    if (browser) {
-      await browser.close()
-    }
+    await page.close()
   }
 })
 
